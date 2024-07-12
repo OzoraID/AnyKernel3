0afb1e1e7d65df78ea0e051d1462439775be0bb2
ZoraXD <136172664+OzoraID@users.noreply.github.com>
2024-07-12 17:56:54 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -117,27 +117,15 @@ kernel_image=$home/kernel/
 if [[ -f $kernel_image/NSE/Image.gz-dtb ]] || [[ -f $kernel_image/SE/Image.gz-dtb ]]; then
 	ui_print "Choose Kernel Version.. "
 	ui_print "x--------------------------------x"
-  sleep 1
-	ui_print "NSE ( No System Ext )"
-  sleep 1
-        ui_print "SE ( System Ext ) "
+ sleep 1
+	ui_print "NSE ( No System Ext : a9-11 )"
+        ui_print "SE ( System Ext : a12-14 ) "
+	ui_print "x--------------------------------x"
+ sleep 1
+	ui_print "Vol+ NSE Version "
+	ui_print "Vol- SE Version "
 	ui_print "x--------------------------------x"
   sleep 1
-	ui_print "1. NSE : a9-a11"
-  ui_print " "
-  sleep 1
-	ui_print "2. SE : a12-a14"
-  sleep 1
-	ui_print "x--------------------------------x"
-	ui_print " Vol+ = NSE, Vol- = SE "
-	ui_print "x--------------------------------x"
-  sleep 1
-	ui_print "1. NSE Version "
-  ui_print " "
-  sleep 1
-	ui_print "2. SE Version "
-	ui_print "x--------------------------------x"
-  sleep 2
   ui_print " "
 
 	if $FUNCTION; then
@@ -163,7 +151,7 @@ if [[ -f $kernel_image/NSE/Image.gz-dtb ]] || [[ -f $kernel_image/SE/Image.gz-dt
 	fi
 
   ui_print " "
-  ui_print "-> Installing KURUMI Kernel "
+  ui_print "-> Installing Kurumi Kernel "
 	ui_print "-> Enjoy... "
 else
 	abort_main;
