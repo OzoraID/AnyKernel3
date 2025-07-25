c5439dd50998f76d7c2e73ae051f5642f843d6fb
NekoTuru10 <kirmansyah9696@gmail.com>
2025-07-25 18:48:56 +0000

    android 16 support

--- anykernel.sh
+++ anykernel.sh
@@ -15,7 +15,7 @@ device.name2=bhima
 device.name3=
 device.name4=
 device.name5=
-supported.versions=11 - 15
+supported.versions=11 - 16
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
